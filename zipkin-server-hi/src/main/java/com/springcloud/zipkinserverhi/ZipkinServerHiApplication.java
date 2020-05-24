e764a95851ca16826a4bb050740434ad7cc36074
wangzheng <718652232@qq.com>
2020-05-24 17:21:12 +0800

    服务链路追踪

---
+++ zipkin-server-hi/src/main/java/com/springcloud/zipkinserverhi/ZipkinServerHiApplication.java
@@ -0,0 +1,53 @@
+package com.springcloud.zipkinserverhi;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ZipkinServerHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerHiApplication.class, args);
+    }
+    private static final Logger LOG = Logger.getLogger(ZipkinServerHiApplication.class.getName());
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject( "http://localhost:8989/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+
+
+}
---
+++ zipkin-server-hi/src/test/java/com/springcloud/zipkinserverhi/ZipkinServerHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.zipkinserverhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
